f896e81f4ad15c9a3a93b806b9bfe91f6fb176f3
Kuihao Chang <41053A041@gms.ndhu.edu.tw>
2020-02-02 00:06:19 +0800

    phpToBrownser_show Button

--- homepage.php
+++ homepage.php
@@ -7,23 +7,110 @@ mysqli_query($con,'SET NAMES utf8');
 
 $sql = "SELECT POS,STATUS FROM booking_info";
 $rst=@mysqli_query($con,$sql);
-while(  )
-
-
-$rst=@mysqli_query($sql,$con);
 if($rst){
-	$pos="";$status="";$status="";
+	$pos="";$status="";$count=0;
 	while( $row = mysqli_fetch_row($rst) ){
-		$ss1=$ss1.$row[0].","; $ss2=$ss2.$row[1].",";
+		++$count;
+		$pos=$pos.$row[0].","; $status=$status.$row[1].",";
 	}
-	echo $ss1."^".$ss2;
+	//echo $count."^".$pos."^".$status."<br>";
 }
 ?>
-
 <?php
-	for($i=1;$i<=10;$i++){
-		echo"<button>{$i}</button>";
+function microtime_float()
+{
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+}
+?>
+<?php
+	$gettime='';
+	$gettime2='';
+	$gettime3='';
+	$bt='';$i=0;
+	$gettime = microtime_float();
+	$bt='';
+	for($i=1;$i<=$count;++$i){
+		$bt=$bt."<button>{$i}</button>";
+		if($i%4==0){$bt=$bt."<br>";}
 	}
+	echo $bt;
+	$gettime2 = microtime_float();
+	$gettime3=$gettime2-$gettime;
+	echo ($gettime3*1000);
+	echo "<p>******<p>"
+?>
+<?php
+	$gettime = microtime_float();
+	$bt='';
+	for($i=1;$i<=$count;++$i){
+		$bt=$bt."<button>{$i}</button>";
+		if($i%4==0){$bt=$bt."<br>";}
+	}
+	echo $bt;
+	$gettime2 = microtime_float();
+	$gettime3=$gettime2-$gettime;
+	echo ($gettime3*1000);
+	echo "<p>******<p>"
+?>
+<?php
+	$gettime = microtime_float();
+	$bt='';
+	for($i=1;$i<=$count;++$i){
+		$bt=$bt."<button>{$i}</button>";
+		if(($i%4)xor 1){$bt=$bt."<br>";}
+	}
+	echo $bt;
+	$gettime2 = microtime_float();
+	$gettime3=$gettime2-$gettime;
+	echo ($gettime3*1000);
+	echo "<p>******<p>"
+?>
+<?php
+	$gettime = microtime_float();
+	$bt='';
+	for($i=1;$i<=$count;++$i){
+		$bt=$bt."<button>{$i}</button>";
+		if(!($i%4)){$bt=$bt."<br>";}
+	}
+	echo $bt;
+	$gettime2 = microtime_float();
+	$gettime3=$gettime2-$gettime;
+	echo ($gettime3*1000);
+	echo "<p>******<p>"
+?>
+<?php
+	$gettime = microtime_float();
+	$bt='';
+	$count2=0;
+	for($i=1;$i<=$count;++$i){
+		$bt=$bt."<button>{$i}</button>";
+		$count2++;
+		if($count2>>2){$count2=0;$bt=$bt."<br>";}
+	}
+	echo $bt;
+	$gettime2 = microtime_float();
+	$gettime3 = $gettime2-$gettime;
+	echo ($gettime3*1000);
+	echo "<p>******<p>"
+?>
+<?php
+	$gettime = microtime_float();
+	$bt='';
+	$count2=0;
+	for($i=1;$i<=$count;++$i){
+		$bt=$bt."<button>{$i}</button>";
+		$count2++;
+		if($count2>>2){$count2&=0;$bt=$bt."<br>";}
+	}
+	echo $bt;
+	$gettime2 = microtime_float();
+	$gettime3 = $gettime2-$gettime;
+	echo ($gettime3*1000);
+	echo "<p>******<p>"
 ?>
 </body>
 </html>
+<script>
+
+</script>
