803a25923f7addd740f241894c1454cf56280c37
Kuihao Chang <41053A041@gms.ndhu.edu.tw>
2020-02-23 22:22:11 +0800

    rebase the commit to protect info.

--- _php_library/connect.php
+++ _php_library/connect.php
@@ -1,4 +1,4 @@
 <?php
-	$con=@mysqli_connect("127.0.0.1", "root", "12345", "booking") or die("NO");
+	$con=@mysqli_connect("127.0.0.1", "root", "Password123", "booking") or die("NO");
 	mysqli_query($con,'SET NAMES utf8');
 ?>
