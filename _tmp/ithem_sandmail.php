f025bcfb59ebf21d0c7148d70cd86c68257f4158
Kuihao Chang <41053A041@gms.ndhu.edu.tw>
2020-03-03 17:18:24 +0800

    learn safty fliter email

--- _tmp/ithem_sandmail.php
+++ _tmp/ithem_sandmail.php
@@ -1,37 +1,4 @@
-<html>
-<body>
-
 <?php
-if (isset($_REQUEST['email']))
-//if "email" is filled out, send email
-  {
-  //send email
-  $email = $_REQUEST['email'] ;
-  $subject = $_REQUEST['subject'] ;
-  $message = $_REQUEST['message'] ;
-  mail( "someone@example.com", "Subject: $subject",
-  $message, "From: $email" );
-  echo "Thank you for using our mail form";
-  }
-else
-//if "email" is not filled out, display the form
-  {
-  echo "<form method='post' action='mail.php'>
-  Email: <input name='email' type='text' /><br />
-  Subject: <input name='subject' type='text' /><br />
-  Message:<br />
-  <textarea name='message' rows='15' cols='40'>
-  </textarea><br />
-  <input type='submit' />
-  </form>";
-  }
-?>
-
-</body>
-</html>
-
-<!--
-<?php/*
     require_once('./phpmailer/PHPMailerAutoload.php');
     $C_name=$_POST['C_name'];
     $C_email=$_POST['C_email'];
@@ -57,6 +24,5 @@ else
         echo "Error: " . $mail->ErrorInfo;
     }else{
         echo "<b>感謝您的留言，您的建議是我們前進的動力。</b>";
-    }*/
+    }
 ?>
--->
--- _tmp/t.php
+++
@@ -1,5 +0,0 @@
-<?php 
-print_r(mysqli_get_client_info ([ mysqli $link = NULL ] ));
-
-
-?>
