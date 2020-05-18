d0d1473ad4129feea1d9dc5ed61badd6cc1ec8b0
Kuihao Chang <41053A041@gms.ndhu.edu.tw>
2020-05-18 13:32:56 +0800

    turn ChoosePage into all php_echo widget and it work

--- _TGW_backup/_Web_Publication_backup/php/choosepage.php
+++ _TGW_backup/_Web_Publication_backup/php/choosepage.php
@@ -1,3 +1,4 @@
+<!-- 原始碼 -->
 <?php if(!empty($datas)):?>
     <?php foreach($datas as $row):?>
         <?php if($row['STATUS']==0): ?>
@@ -15,6 +16,7 @@
 <?php endif; ?> 
 
 <?php
+// 畫布的部分
 if(!empty($datas)){
     foreach($datas as $row){
         if($row['STATUS']==0){
@@ -43,3 +45,46 @@ if(!empty($datas)){
 else{
     echo "<h3>PHP's get_position_and_status() return error!</h3>";
 }
+?>
+
+<?php
+// php page全包碼
+$zone=$_GET['zone'];
+$datas=get_position_and_status($zone);
+$src = "src=\"http://127.0.0.1/wp-content/uploads/2020/05/test_breakfast_part_".$zone.".jpg\" ";
+echo "<div id=\"id_container_picture\" class=\"container grid Bob\">";
+echo "<img class=\"layer_picture box_picture\" ".$src." alt=\" This is a painting picture.\">\";";
+echo "    <div id=\"id_container_parts\" class=\"container grid Alice\">";
+
+if(!empty($datas)){
+    foreach($datas as $row){
+        if($row['STATUS']==0){
+            echo "<div class='layer_parts box_parts' onclick=\"LinkTo_BookingPage('".$row['POS']."')\" 
+            onmouseenter=\"show_state(this, '".$row['STATUS']."', '".$row['POS']."')\" 
+            onmouseleave=\"set_origin(this ,'".$row['POS']."')\">";
+            echo $row['POS'];
+            echo "</div>";
+        }
+        elseif($row['STATUS']==1){
+            echo "<div class='layer_parts box_parts lock' 
+            onmouseenter=\"show_state(this, '".$row['STATUS']."', '".$row['POS']."')\" 
+            onmouseleave=\"set_origin(this ,'".$row['POS']."')\">";
+            echo $row['POS'];
+            echo "</div>";
+        }
+        elseif($row['STATUS']==-1){
+            echo "<div class='layer_parts box_parts lock' 
+            onmouseenter=\"show_state(this, '".$row['STATUS']."', '".$row['POS']."')\" 
+            onmouseleave=\"set_origin(this ,'".$row['POS']."')\">";
+            echo $row['POS'];
+            echo "</div>";
+        }
+    }
+}
+else{
+    echo "<h3>PHP's get_position_and_status() return error!</h3>";
+}
+
+echo "    </div>";
+echo "</div>";
+echo "<input type=\"button\" onclick=\"location.href='HomePage.php'\" value=\"回到上一頁\">";
